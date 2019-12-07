d483a7c7528328fcb5942ad87f1f4ef048357dd2
Oscar Caballero <ocaballeror@tutanota.com>
2019-12-07 02:05:35 +0100

    Complete day 22 part 2

--- 22/sol2.rb
+++ 22/sol2.rb
@@ -4,9 +4,15 @@ ROCKY = 0
 WET = 1
 NARROW = 2
 
-NEITHER = 0
-CLIMBING = 1
-TORCH = 2
+NEITHER = 3
+CLIMBING = 4
+TORCH = 5
+
+TOOLS = {
+  ROCKY => [CLIMBING, TORCH],
+  NARROW => [TORCH, NEITHER],
+  WET => [NEITHER, CLIMBING],
+}
 
 
 open("input") do |file|
@@ -36,102 +42,66 @@ end
 def adjacent(point)
   [[-1, 0], [0, -1], [1, 0], [0, 1]].map {
     |move| [point[0] + move[0], point[1] + move[1]] }.select {
-    |p| $types.include?(p) and p[0] >= 0 and p[1] >= 0 }
+      |p| $types.include?(p) and p[0] >= 0 and p[1] >= 0 }
 end
 
-
-def get_distance(point, other, tool)
-  point_type = $types[point]
+def move_choices(point, other, tool)
+  current_type = $types[point]
   other_type = $types[other]
-  return 1, tool if point_type == other_type
+  tool_choices = TOOLS[other_type]
 
-  tools = {
-    [ROCKY, WET] => CLIMBING,
-    [ROCKY, NARROW] => TORCH,
-    [WET, NARROW] => NEITHER,
-  }
-  proper_tool = tools[[point_type, other_type]]
-  if proper_tool.nil?
-    proper_tool = tools[[other_type, point_type]]
-  end
-  if proper_tool == tool
-    return 1, tool
+  choices = []
+  if tool_choices.include? tool
+    choices << [1, tool]
   else
-    return 8, proper_tool
+    tool_choices.each do |t|
+      next if !TOOLS[current_type].include? t
+      choices << [8, t]
+    end
   end
+  choices
 end
 
-
-# TODO prune if a path has been found and is shorter
-# TODO end if we already found a path?
-# TODO Use a heap?
-# TODO Carry the path around
-#
-#
 
 def dijkstra(origin, target)
-  to_visit = [[origin, 0, [], TORCH]]
-  distances = {origin => [0, []]}
-  seen = Set.new
-  until to_visit.empty?
-    # node = to_visit.min_by {|x| x[1]}
-    node = to_visit.min_by {|x| x[1]}
-    position, distance, tool = node
-    # break if distances.include?(target) and distance > distances[target][0]
-    to_visit.delete(node)
-    adjacent(position).each do |neighbor|
-      time, newtool = get_distance(position, neighbor, tool)
-      time += distance
-      time += 7 if neighbor == target and newtool != TORCH
-      known = distances[neighbor]
-      if known.nil? or time < known[0]
-        distances[neighbor] = time, position
-      end
-      if !seen.include?(neighbor)
-        if !to_visit.any? { |field| neighbor == field[0] }
+  to_visit = [[origin, 0, TORCH]]
+  distances = Hash.new(Float::INFINITY)
+  best = Float::INFINITY
+  chunk = []
+  until to_visit.empty? and chunk.empty?
+    to_visit.sort_by! { |x| x[1] }
+    chunk = to_visit.shift(300)
+    print "Best: #{best}, Remaining: #{to_visit.size + chunk.size}    \r"
+
+    chunk.each do |position, distance, tool|
+      adjacent(position).each do |neighbor|
+        move_choices(position, neighbor, tool).each do |time, newtool|
+          time += distance
+          next if time > best
+          next if time >= distances[[newtool, neighbor]]
+          if neighbor == target
+            time += 7 if newtool != TORCH
+            best = time if time < best
+            next
+          end
+
+          distances[[newtool, neighbor]] = time
           to_visit << [neighbor, time, newtool]
         end
       end
     end
-    seen << position
   end
-  # distances[target]
-  distances
+  puts
+  best
 end
-
-def shortest_path(origin, target)
-  distances = dijkstra(origin, target)
-  puts(distances.to_s)
-  ret = []
-  t = distances[target][1]
-  while t != origin
-    ret << t
-    t = distances[t][1]
-  end
-  return distances[target], ret.reverse + [target]
-end
-
-
-def print_thing(path)
-  tool = TORCH
-  last = [0, 0]
-  d = 0
-  path.each do |point|
-    distance, tool = get_distance(last, point, tool)
-    d += distance
-    puts("#{d} #{point[0]} #{point[1]}")
-    last = point
-  end
-end
-
 
 
 indexes = []
 $types = {}
 answer = 0
-(0..$target[1] + 7).each do |y|
+(0..$target[1] + 30).each do |y|
   indexes[y] = [0] * $target[1]
-  (0..$target[0] + 7).each do |x|
+  (0..$target[0] + 30).each do |x|
     index = geologic_index(x, y, indexes)
     index = (index + $depth) % 20183
     indexes[y][x] = index
@@ -141,4 +111,5 @@ answer = 0
   end
 end
 
-print_thing(shortest_path([0, 0], $target)[1])
+sol = dijkstra([0, 0], $target)
+puts "Shortest distance: #{sol}"
